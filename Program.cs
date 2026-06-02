7351d9bb88b6620087ffa8345429050042425b2d
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-06-02 11:13:01 +0300

    Allow migrations to be read for deployment

--- Program.cs
+++ Program.cs
@@ -16,6 +16,8 @@ builder
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
+
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -40,5 +42,11 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(name: "default", pattern: "{controller=Account}/{action=Login}/{id?}");
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
